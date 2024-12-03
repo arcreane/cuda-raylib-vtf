a9b2207dc9f084cf333f6a7909adcef01e4b8cfe
Victor <victor.guermonprez@ipsa.fr>
2024-12-03 22:48:36 +0100

    maj structure

--- ExpressGame/renderer.hpp
+++ ExpressGame/renderer.hpp
@@ -1,10 +1,7 @@
 #include "raylib.h"
 
 // Fonction pour initialiser la fen�tre et les ressources
-void InitGameWindow(int screenWidth, int screenHeight) {
-    InitWindow(screenWidth, screenHeight, "A game about particles");
-}
-
+void InitGameWindow(int screenWidth, int screenHeight);
 // Fonction pour afficher l'�cran de victoire
 void DrawVictoryScreen(int screenWidth, int screenHeight);
 
