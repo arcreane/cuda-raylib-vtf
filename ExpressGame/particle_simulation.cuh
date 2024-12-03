69394dfe5b1c0ee8c6fcf3ed2681ac2dd0260083
Thibault <thibault.lecordier91@gmail.com>
2024-12-03 21:05:28 +0100

    Structure du projet

---
+++ ExpressGame/particle_simulation.cuh
@@ -0,0 +1,24 @@
+#include "raylib.h"
+#include <vector>
+#include <cuda_runtime.h>
+#include <curand_kernel.h>
+#include <cmath>
+
+// Structure pour une particule
+struct Particle {
+    float x, y;         // Position
+    float dx, dy;       // Direction de déplacement
+    unsigned char r, g, b, a; // Couleur
+    bool active;        // Statut de la particule (active/inactive)
+};
+
+#ifndef PARTICLE_SIMULATION_CUH
+// Prototypes de fonctions
+Particle* InitializeParticlesGPU(int numParticles, int screenWidth, int screenHeight);
+void UpdateParticles(Particle* deviceParticles, int numParticles, float mouseX, float mouseY, float targetX,
+    float targetY, float targetRadius, bool attract, float influenceRadius,
+    int* deviceScore, float speed);
+#endif // !PARTICLE_SIMULATION_CUH
+
+
+
